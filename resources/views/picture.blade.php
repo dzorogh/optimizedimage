2b71effd215a8311da8dbdc79d50ea88415e0310
dzorogh <dzorogh@gmail.com>
2020-10-12 15:51:23 +0300

    Now you can select - lazy or not

--- resources/views/picture.blade.php
+++ resources/views/picture.blade.php
@@ -124,6 +124,7 @@
             @if ($lazy)
                 src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
             @endif
+
             {{ $lazy ? 'data-src' : 'src' }}="{{
                 optimized($src, [
                     'w' => $width ?? null,
