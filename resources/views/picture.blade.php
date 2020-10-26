163a3d702cfaf34b3509b7e33d8940508dbfa3b8
dzorogh <dzorogh@gmail.com>
2020-10-26 13:08:12 +0300

    Fix placeholder

--- resources/views/picture.blade.php
+++ resources/views/picture.blade.php
@@ -91,7 +91,7 @@
 
     <img
             @if ($lazy)
-                src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 {{ $width }} {{ $height }}'%3E%3C/svg%3E"
+                src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
             @endif
 
             {{ $lazy ? 'data-src' : 'src' }}="{{
